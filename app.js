4f773bf9f6bc848e9aa49d8ad473881e4935f33d
tmzzz <temir1201@gmail.com>
2019-12-07 13:06:27 +0300

    pug added

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.set('view engine', 'pug');
+
 app.listen(3001, () => {
     console.log('listen port 3001');    
 });
 
 app.get('/', (req,res) => {
-    res.render('index.html');
+    res.render('main', {
+        foo : 4,
+        bar : 7
+    });
 });
